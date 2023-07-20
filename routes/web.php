804339ba4d352d0319bd6d80262d92634e55276a
Anderson <billbardales@gmail.com>
2023-07-20 08:15:06 -0500

    Autenticación con google

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\auth;
-use App\Http\Controllers\AppointmentController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\PaystackController;
-use App\Http\Controllers\ServiceController;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,24 +21,35 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-auth::routes();
+Route::get('/google-auth/redirect', function () {
+    return Socialite::driver('google')->redirect();
+});
+ 
+Route::get('/google-auth/callback', function () {
+    $user_google = Socialite::driver('google')->user();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    $user = User::updateOrCreate([
+        'google_id' => $user_google->id,
+    ],[
+        'name' => $user_google->name,
+        'email' => $user_google->email,
+    ]);
+
+    Auth::login($user);
+
+    return redirect('/dashboard');
+ 
+    // $user->token
 });
 
-Route::middleware(['auth'])->group(function () {
-    Route::resource('services', ServiceController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('/appointments', [AppointmentController::class, 'index'])->name('appointments.index');
-Route::get('/services/{service}/appointments/create', [AppointmentController::class, 'create'])->name('appointments.create');
-Route::post('/services/{service}/appointments', [AppointmentController::class, 'store'])->name('appointments.store');
-Route::delete('/appointments/{appointment}', [AppointmentController::class, 'destroy'])->name('appointments.destroy');
 
-Route::post('/pay', [PaystackController::class, 'redirectToGateway'])->name('pay');
-Route::get('/payment/callback', [PaystackController::class, 'handleGatewayCallback']);
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+require __DIR__.'/auth.php';
