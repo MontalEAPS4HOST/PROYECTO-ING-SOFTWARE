3a9704b2b71ab9b5d3d57ef5aa902852de5c4142
Anderson <billbardales@gmail.com>
2023-08-30 10:02:04 -0500

    calendario y boton de reserva de cita

---
+++ resources/views/calendar_table.blade.php
@@ -0,0 +1,104 @@
+<table border="1">
+    <!-- Cabecera de la tabla -->
+    <thead>
+        <tr>
+            <th>Domingo</th>
+            <th>Lunes</th>
+            <th>Martes</th>
+            <th>Miércoles</th>
+            <th>Jueves</th>
+            <th>Viernes</th>
+            <th>Sábado</th>
+        </tr>
+    </thead>
+    <!-- Cuerpo de la tabla -->
+    <tbody>
+        <tr>
+            <td></td> <!-- 1 vacío -->
+            <td></td> <!-- 2 vacío -->
+            <td>1</td> <!-- 3 -->
+            <td>2</td> <!-- 4 -->
+            <td>3</td> <!-- 5 -->
+            <td>4</td> <!-- 6 -->
+            <td>5</td> <!-- 7 -->
+        </tr>
+        <tr>
+            <td>6</td> <!-- 8 -->
+            <td>7</td> <!-- 9 -->
+            <td>8</td> <!-- 10 -->
+            <td>9</td> <!-- 11 -->
+            <td>10</td> <!-- 12 -->
+            <td>11</td> <!-- 13 -->
+            <td>12</td> <!-- 14 -->
+        </tr>
+
+        <tr>
+            <td>13</td> <!-- 8 -->
+            <td>14</td> <!-- 9 -->
+            <td>15</td> <!-- 10 -->
+            <td>16</td> <!-- 11 -->
+            <td>17</td> <!-- 12 -->
+            <td>18</td> <!-- 13 -->
+            <td>19</td> <!-- 14 -->
+        </tr>
+        <tr>
+            <td>20</td> <!-- 8 -->
+            <td>21</td> <!-- 9 -->
+            <td>22</td> <!-- 10 -->
+            <td>23</td> <!-- 11 -->
+            <td>24</td> <!-- 12 -->
+            <td>25</td> <!-- 13 -->
+            <td>26</td> <!-- 14 -->
+        </tr>
+        <tr>
+            <td>27</td> <!-- 8 -->
+            <td>28</td> <!-- 9 -->
+            <td>29</td> <!-- 10 -->
+            <td>30</td> <!-- 11 -->
+            <td>31</td> <!-- 12 -->
+            <td></td> <!-- 13 -->
+            <td></td> <!-- 14 -->
+        </tr>
+        <!-- Más filas aquí... -->
+    </tbody>
+</table>
+
+
+<style>
+    /* Estilos del contenedor */
+    .calendar-container {
+        width: 80%;
+        margin: auto;
+        text-align: center;
+    }
+
+    /* Estilos de la tabla */
+    table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+
+    /* Estilos de la cabecera */
+    thead th {
+        background-color: #4CAF50;
+        color: white;
+        padding: 15px;
+    }
+
+    /* Estilos de las celdas */
+    tbody td {
+        border: 1px solid #ddd;
+        padding: 15px;
+    }
+
+    /* Estilos de las celdas vacías */
+    .empty {
+        background-color: #f2f2f2;
+    }
+
+    /* Estilos del título */
+    h1 {
+        color: #333;
+        margin-bottom: 20px;
+    }
+</style>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,5 @@
+
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -6,12 +8,24 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <!-- Cuadro que ocupa el 70% -->
+        <div style="width: calc(70% - 20px); float: left; margin: 10px; background-color: #ccc;">
+            @include('calendar_table')
+        </div>
+
+        <!-- Cuadro que ocupa el 30% -->
+        <div style="width: calc(30% - 20px); float: left; margin: 10px; background-color: #eee;">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("Estás logueado!") }}
+                    <button type="button" onclick="alert('Cita Reservada')">Reservar Cita</button>
+
                 </div>
             </div>
         </div>
+
+        <div style="clear: both;"></div>
     </div>
+    </div>
+
 </x-app-layout>
